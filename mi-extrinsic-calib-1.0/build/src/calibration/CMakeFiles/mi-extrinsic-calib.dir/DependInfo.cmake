fbcbc361a6963962775323b2b4578efa6c93a266
Nikita <mirolyubov_n@yahoo.co.uk>
2017-10-15 19:37:52 +0100

    initial commit of code

---
+++ mi-extrinsic-calib-1.0/build/src/calibration/CMakeFiles/mi-extrinsic-calib.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/calibration/Calibration.cpp" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/calibration/CMakeFiles/mi-extrinsic-calib.dir/Calibration.cpp.o"
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/calibration/Calibration_deprecated.cpp" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/calibration/CMakeFiles/mi-extrinsic-calib.dir/Calibration_deprecated.cpp.o"
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/calibration/main.cpp" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/calibration/CMakeFiles/mi-extrinsic-calib.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv2"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "../src/common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/common/config.c" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/config.c.o"
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/common/fasttrig.c" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/fasttrig.c.o"
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/common/gsl_util.c" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/gsl_util.c.o"
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/common/small_linalg.c" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/small_linalg.c.o"
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/common/ssc.c" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/ssc.c.o"
+  "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/src/common/timestamp.c" "/home/nikita/Desktop/project/mi-extrinsic-calib-1.0/build/src/common/CMakeFiles/common.dir/timestamp.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv2"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
